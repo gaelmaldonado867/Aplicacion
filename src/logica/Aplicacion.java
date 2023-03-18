ffa50f9007e36277fb5c782408d449f1abcbedf1
Gael <Gael@DESKTOP-OSGTSG8>
2023-03-17 20:18:45 -0600

    Inicio Aplicacion

---
+++ src/logica/Aplicacion.java
@@ -0,0 +1,25 @@
+
+package logica;
+
+import igu.Pantalla;
+
+/**
+ *
+ * @author Gael
+ */
+public class Aplicacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //Crear variable de pantalla
+        Pantalla panta = new Pantalla();
+        //LLamar pantalla
+        panta.setVisible(true);
+        //Abrir pantalla
+        panta.setLocationRelativeTo(null);
+        
+    }
+    
+}
